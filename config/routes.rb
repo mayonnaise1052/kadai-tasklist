72fed6c80a5299cb7b7223c67f54f3f20c92c8b2
Ubuntu <ubuntu@ip-172-31-7-67.ap-northeast-1.compute.internal>
2024-01-07 00:49:35 +0000

    delete users#index

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:show, :create]
 
   resources :tasks
 end
